6558d64bf28eb85ed3c14b3c58629f660bc821be
abhishek <abhishekmanglam800@gmail.com>
2023-11-19 15:32:30 +0530

    Netflix Clone Commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,41 @@
+import { NextPageContext } from "next"
+import { getSession } from "next-auth/react"
+import Navbar from "@/components/Navbar";
+import Billboard from "@/components/Billboard";
+import useInfoModalStore from "../../hooks/useInfoModalStore";
+import InfoModal from "@/components/InfoModal";
+import MovieList from "@/components/MovieList";
+import useMovieList from "../../hooks/useMovieList";
+import useFavorites from '../../hooks/useFavorites'
+
+export async function getServerSideProps(context:NextPageContext){
+  const session =await getSession(context);
+  if(!session){
+    return {
+      redirect:{
+        destination:'/auth',
+        permanant:false
+      }
+    }
+  }
+  return{
+    props:{}
+  }
+}
+
+export default function Home() {
+  const { data: movies = [] } = useMovieList();
+  const { data: favorites = [] } = useFavorites();
+  const {isOpen, closeModal} = useInfoModalStore();
+  return (
+  <>
+  <InfoModal visible={isOpen} onClose={closeModal} />
+  <Navbar/>
+  <Billboard/>
+  <div className="pb-40">
+     <MovieList title="Trending Now" data={movies} />
+     <MovieList title="My List" data={favorites} />
+  </div>
+  </>
+  )
+}
